f836f2b422ecdd6d4a52da67252dbaa6415f20aa
minh phuong <phuong.tm225381@sis.hust.edu.vn>
2025-09-08 11:34:08 +0700

    add validation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MyLogger } from './utils/logger';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,14 @@ async function bootstrap() {
   const documentFactory = () =>
     SwaggerModule.createDocument(app, config, { ignoreGlobalPrefix: true });
   SwaggerModule.setup('api', app, documentFactory);
+
+  //validation data
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 5000, () => {
     logger.log(`Server is running on: ` + process.env.PORT);
